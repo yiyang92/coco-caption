a937a7154541213a4860e321c7d95a78bd5eba03
Nikolai Zakharov <1556712541@qq.com>
2019-02-18 10:51:30 +0800

    Update eval_caption.py

--- eval_caption.py
+++ eval_caption.py
@@ -2,14 +2,20 @@ from __future__ import print_function
 
 import argparse
 
+import json
 
-def main(*args, **kwargs):
-    
+from pycocotools.coco import COCO
+from pycocoevalcap.eval import COCOEvalCap
+
+
+def main(args):
+    coco = COCO(args.annot_cap)
+    cocoRes = coco.loadRes(args.res_cap)
+    cocoEval = COCOEvalCap(coco, cocoRes)
+    cocoEval.evaluate()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Caption evaluation")
-    parser.add_argument("input_cap", help="Input caption json file")
     parser.add_argument("annot_cap", help="Ground truth annotation file")
-    args = parser.parse_args()
-    main()
+    parser.add_argument("res_cap", help="Input caption json file")
